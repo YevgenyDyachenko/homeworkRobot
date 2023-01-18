0c213aea1ec762cc3deaa721a262328244c833be
YevgenyDyachenko <yevgeny.dyachenko@gmail.com>
2023-01-18 19:22:51 +0200

    rd

--- src/com/ua/robot/homework0/Main.java
+++ src/com/ua/robot/homework0/Main.java
@@ -2,6 +2,6 @@ package com.ua.robot.homework0;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I'm starting a Java course at robot_dreams and already know how to use Git");
+        System.out.println("I'm starting a Java course at robot_dreams and already know how to use Git!");
     }
 }
